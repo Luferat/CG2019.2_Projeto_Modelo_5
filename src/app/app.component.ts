88ec54dda37de7ff3378e0ced99e80472dddf877
André Luferat <Andr Luferat>
2021-02-23 16:17:54 -0300

    Final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Início', url: '/home', icon: 'home' },
     { title: 'Notícias', url: '/news', icon: 'newspaper' },
     { title: 'Faça Contato', url: '/contacts', icon: 'chatbubbles' },
     { title: 'Sobre', url: '/about', icon: 'information-circle' }
--- src/app/pages/news/news.page.ts
+++ src/app/pages/news/news.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+// 1. Importa dependências
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-news',
   templateUrl: './news.page.html',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsPage implements OnInit {
 
-  constructor() { }
+  // 3. Atributos
+  private apiKey = 'f6b1d5dc3f4c47c483446e353984603f'; // Obtenha de newsapi.org
+  private apiQuery = 'flores'; // Palavra chave das notícias
+  private apiItens = 10; // Quantidad e notícias à exibir (máx. 20)
+  public apiURL = `https://newsapi.org/v2/everything?apiKey=${this.apiKey}&language=pt&q=${this.apiQuery}`;
+  public newsList: any; // Armazena as notícias obtidas da API
+
+  constructor(
+
+    // 2. Injeta dependência
+    private http: HttpClient
+  ) { }
 
   ngOnInit() {
+
+    // 4. Obtendo as notícias da API (JSON)
+    this.http.get(this.apiURL).subscribe(
+      (data: any) => {
+
+        // 5. Atribui à view de notícias
+        this.newsList = data.articles.slice(
+          0,
+          this.apiItens
+        );
+      }
+    );
+  }
+
+  // 6. Acessa o site com a notícias completa
+  readNews(link: any) {
+    window.open(link);
+    return false;
   }
 
 }
